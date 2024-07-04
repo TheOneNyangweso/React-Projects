f2fadebbe9ff499e5a0775a8266fc8a1caf6bd89
TheOneNyangweso <morurisammy5@gmail.com>
2024-07-04 23:50:01 +0300

    single selection component created

---
+++ projects/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordion from './components/Accordion/Index';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/src/components/Accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: '1',
+    question: 'What are accordion components?',
+    answer: '...',
+  },
+  {
+    id: '2',
+    question: 'What are they used for?',
+    answer: '...',
+  },
+  {
+    id: '3',
+    question: 'Describe the accordion as a music instrument',
+    answer: '...',
+  },
+  {
+    id: '4',
+    question: 'Can I create an accordion component with a different framework',
+    answer: '...',
+  },
+];
+
+export default data;
